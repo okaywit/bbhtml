2cb03517d70dc058614c2dbb6d726f3f5d8b35aa
大辉Face <okaywit@163.com>
2015-07-29 13:26:03 +0800

    统计+缓存

--- js/socket.js
+++ js/socket.js
@@ -1,39 +1,22 @@
-/**
- * Created by Administrator on 2015/6/18.
- */
 var Server = {};
 var isGo = true;
 Server.socket = null;
 var host = window.location.host;
-
 Server.connect = (function(host){
-
     Server.socket = new WebSocket(host);
-    Server.socket.onopen=function(){
-    	
-    };
-    Server.socket.onmessage=function(message){
-        
-    };
-    Server.socket.onclose=function(){
-        showError();
-    };
-    Server.socket.onerror=function(e){
-        showError();
-    };
+    Server.socket.onopen=function(){   	};
+    Server.socket.onmessage=function(message){};
+    Server.socket.onclose=function(){ showError();};
+    Server.socket.onerror=function(e){showError();};
 });
-
 window.onbeforeunload = onbeforeunload_handler;
 window.onunload = onunload_handler;
 function onbeforeunload_handler(){
     isGo = false;
 }
- 
 function onunload_handler(){
     isGo = false;
 }
-
-
 function showError(){
     if(isGo){
         location.href='/error.html';
